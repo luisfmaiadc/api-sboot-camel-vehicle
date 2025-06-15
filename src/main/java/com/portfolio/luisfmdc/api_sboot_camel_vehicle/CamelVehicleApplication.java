5dc246d4bd285473502ac62b1d070dcdf32f3e90
luisfmaiadc <lf.mcosta23@gmail.com>
2025-06-15 16:35:52 -0300

    Inicialização do versionamento do projeto.

---
+++ src/main/java/com/portfolio/luisfmdc/api_sboot_camel_vehicle/CamelVehicleApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.api_sboot_camel_vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelVehicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/luisfmdc/api_sboot_camel_vehicle/CamelVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.api_sboot_camel_vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
